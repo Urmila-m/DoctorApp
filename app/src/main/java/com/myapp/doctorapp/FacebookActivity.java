da34102d2f5465339ee378baf478ad6f0501d307
unknown <urmi.mhrz@gmail.com>
2019-02-28 14:25:33 +0545

    remaining layouts added

--- app/src/main/java/com/myapp/doctorapp/FacebookActivity.java
+++ app/src/main/java/com/myapp/doctorapp/FacebookActivity.java
@@ -1,14 +1,19 @@
 package com.myapp.doctorapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class FacebookActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.birthday_selector_layout);
+        setContentView(R.layout.doctor_information_layout);
+
     }
+
 }
